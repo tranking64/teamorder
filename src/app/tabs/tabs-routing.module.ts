25eb5ee90ae3881f8b2f4dac9ec1ba6cf83503a0
dmnktrn <d.tran@htlkrems.at>
2022-01-10 09:26:31 +0100

    added fourth tab
    added icons and routing

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'tab4',
+        loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -30,7 +34,12 @@ const routes: Routes = [
     path: '',
     redirectTo: '/tabs/tab1',
     pathMatch: 'full'
-  }
+  },
+  {
+    path: 'tabs/tab4',
+    redirectTo: '/tab4',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
